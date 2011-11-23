d36d4a74c41de0344f19fe36863eadfd00ac9b04
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-11-23 21:43:25 +0400

    Add scripts for compiling photon flashable images.

--- mknbh.sh
+++ mknbh.sh
@@ -5,12 +5,14 @@ export OBJCOPY=${CROSS_COMPILE}objcopy
 
 pushd .
 cd /home/alexander/handhelds/htc/lk-msm7200a-htc-wince
-bash build.sh
+bash photon.sh
 popd
-cp /home/alexander/handhelds/htc/lk-msm7200a-htc-wince/build-msm7200a_htc_wince/lk.bin .
+rm lk.bin
+cp /home/alexander/handhelds/htc/lk-msm7200a-htc-wince/build-msm7227_htc_wince/lk.bin .
 rm *.NBH
 rm out.nb
+rm tinboot tinboot.o
 $AS tinboot.S -o tinboot.o
 $OBJCOPY tinboot.o -O binary tinboot
 ./generate
-yang -F POOPLOADER.NBH -f out.nb -t 0x400 -s 64 -d KOVS***** -c 11111111 -v 1.0.XDAPOOP -l WWE
+yang -F HDLOADER.NBH -f out.nb -t 0x400 -s 64 -d PB9210000 -c 11111111 -v 1.42.1337 -l WWE
